834c0330ae70b9ba8432248accf8ee5466753a1f
yutannihilation <yutani.ini@gmail.com>
2015-08-09 01:15:43 +0900

    add pagenation to qiita_api

--- R/util.r
+++ R/util.r
@@ -40,14 +40,39 @@ qiita_payload <- function(body = NULL, title = NULL, tags = NULL, gist = NULL, p
 #' @param path path of API
 #' @param payload JSON payload
 #' @param query URI quer
+#' @param per_page number of items/tags/users/etc. in one page
+#' @param page number of the page (This param is ignored when get_all is TRUE)
+#' @param get_all whethear or not to get all items/tags/users/etc.
 #' @export
-qiita_api <- function(verb, url, path, token, payload = NULL, query = NULL) {
+qiita_api <- function(verb, url, path, token, payload = NULL, query = NULL, per_page = 100, page = 1, get_all = FALSE) {
+  if(any(c("per_page", "page") %in% names(query))) warning("per_page and page in query is overwritten")
+
+  if(is.null(query)) query <- list()
+  query$per_page <- per_page
+  query$page     <- ifelse(get_all, 1, page) #When getting all pages, attempts should start from page 1
+
+  header <- httr::add_headers(`Content-Type`  = "application/json",
+                              `Authorization` = paste("Bearer", token))
+
   res <- httr::VERB(verb, url = url, path = path,
-             config = httr::add_headers(`Content-Type`  = "application/json",
-                                        `Authorization` = paste("Bearer", token)),
-             body = payload, query = query)
+             config = header, body = payload, query = query)
 
   # FIXME: specifying `type` is a temporal workaround to avoid error with mime::guess_type()
-  httr::content(res, encoding = "UTF-8", type = "application/json")
-}
+  result <- httr::content(res, encoding = "UTF-8", type = "application/json")
 
+  total_count <- res$headers$`total-count`
+  cat("total count is", total_count)
+
+  if(total_count > per_page && get_all) {
+    for(p in seq(2, trunc(total_count/per_page)+1)) {
+      query$page <- p
+
+      res <- httr::VERB(verb, url = url, path = path,
+                        config = header, body = payload, query = query)
+
+      result <- c(result, httr::content(res, encoding = "UTF-8", type = "application/json"))
+    }
+  }
+
+  result
+}
