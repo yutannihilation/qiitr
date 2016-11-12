a8f137d880610868a69bf64a807c4177a12529e7
Hiroaki Yutani <yutani.ini@gmail.com>
2016-11-12 18:41:18 +0900

    skip on cran

--- tests/testthat/test-qiita_get_items.R
+++ tests/testthat/test-qiita_get_items.R
@@ -1,5 +1,7 @@
 context("qiita_get_items")
 
+skip_on_cran()
+
 test_that("qiita_get_items", {
   expect_equal(qiita_get_items(tag_id = "R", per_page = 3L, page_limit = 1L),
                qiita_get_items(tag_id = "R", per_page = 1L, page_limit = 3L))
