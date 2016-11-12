84d1e681092c602a9b833a3634be31eea0b0bea4
Hiroaki Yutani <yutani.ini@gmail.com>
2016-11-12 19:10:08 +0900

    update documentation for tag

--- R/tag.r
+++ R/tag.r
@@ -1,13 +1,27 @@
 #' Qiita Tag API
 #'
-#' Get tags via Qiita API.
+#' Get, follow or unfollow tags via Qiita API.
 #'
 #' @name tag
-#' @param user_id User ID.
-#' @param tag_id Tag ID.
+#' @param tag_id Tag ID (e.g. \code{"R"}, \code{"dplyr"}).
+#' @param user_id User ID (e.g. \code{"yutannihilation"}).
 #' @param per_page Number of items per one page.
 #' @param page_offset Page offset.
 #' @param page_limit Max number of pages to aquire.
+#' @examples
+#' \dontrun{
+#' # get a tag by Tag IDs
+#' qiita_get_tags(tag_id = "R")
+#'
+#' # get tags by user ID
+#' qiita_get_tags(user_id = "yutannihilation")
+#'
+#' # follow a tag
+#' qiita_follow_tag("RStudio")
+#'
+#' # unfollow a tag
+#' qiita_unfollow_tag("RStudio")
+#' }
 #' @export
 qiita_get_tags <- function(tag_id = NULL, user_id = NULL,
                           per_page = 100L, page_offset = 0L, page_limit = 1L) {
@@ -23,8 +37,7 @@ qiita_get_tags <- function(tag_id = NULL, user_id = NULL,
 
   # Get an tag by ID (No pagenation is needed)
   if(!is.null(tag_id)){
-    path <- sprintf("/api/v2/tags/%s", tag_id)
-    result <- qiita_api("GET", path = path)
+    result <- purrr::map(tag_id, qiita_get_single_tag_by_id)
     return(result)
   }
 
@@ -35,8 +48,11 @@ qiita_get_tags <- function(tag_id = NULL, user_id = NULL,
   }
 }
 
-#' @rdname tag
-#' @export
+qiita_get_single_tag_by_id <- function(tag_id) {
+  path <- sprintf("/api/v2/tags/%s", tag_id)
+  qiita_api("GET", path = path)
+}
+
 qiita_get_tags_followed_by_user <- function(user_id,
                                     per_page = 100L, page_offset = 0L, page_limit = 1L) {
   path <- sprintf("/api/v2/users/%s/following_tags", user_id)
@@ -46,23 +62,30 @@ qiita_get_tags_followed_by_user <- function(user_id,
 
 #' @rdname tag
 #' @export
-qiita_delete_tags_following <- function(tag_id) {
+qiita_follow_tag <- function(tag_id) {
+  if(!purrr::is_scalar_character(tag_id)) stop("tag_id must be an scalar character!")
+
+  path <- sprintf("/api/v2/tags/%s/following", tag_id)
+  qiita_api("PUT", path = path)
+}
+
+
+#' @rdname tag
+#' @export
+qiita_unfollow_tag <- function(tag_id) {
+  if(!purrr::is_scalar_character(tag_id)) stop("tag_id must be an scalar character!")
+
   path <- sprintf("/api/v2/tags/%s/following", tag_id)
   qiita_api("DELETE", path = path)
 }
 
-#' @rdname tag
-#' @export
-qiita_get_tags_following <- function(tag_id,
-                                per_page = 100L, page_offset = 0L, page_limit = 1L) {
-  path <- sprintf("/api/v2/tags/%s/following", tag_id)
-  qiita_api("GET", path = path,
-            per_page = per_page, page_offset = page_offset, page_limit = page_limit)
-}
-
-#' @rdname tag
-#' @export
-qiita_put_tags_following <- function(tag_id) {
-  path <- sprintf("/api/v2/tags/%s/following", tag_id)
-  qiita_api("PUT", path = path)
-}
+# This API seems not to work.
+#
+# #' @rdname tag
+# #' @export
+# qiita_get_tags_following <- function(tag_id,
+#                                 per_page = 100L, page_offset = 0L, page_limit = 1L) {
+#   path <- sprintf("/api/v2/tags/%s/following", tag_id)
+#   qiita_api("GET", path = path,
+#             per_page = per_page, page_offset = page_offset, page_limit = page_limit)
+# }
