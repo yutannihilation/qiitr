1a67acd6b565a168df79c37de27fd5fa7a2d8eed
Hiroaki Yutani <yutani.ini@gmail.com>
2020-05-16 13:12:26 +0900

    Skip most of the tests on CI

--- tests/testthat/test-qiita_get_items.R
+++ tests/testthat/test-qiita_get_items.R
@@ -3,12 +3,16 @@ context("qiita_get_items")
 
 test_that("qiita_get_items by tag ID", {
   skip_on_cran()
+  skip_on_ci()
+
   expect_equal(qiita_get_items(tag_id = "R", per_page = 3L, page_limit = 1L),
                qiita_get_items(tag_id = "R", per_page = 1L, page_limit = 3L))
 })
 
 test_that("qiita_get_items by item ID", {
   skip_on_cran()
+  skip_on_ci()
+
   item <- qiita_get_items(item_id = "7a78d897810446dd6a3b")
   expect_equal(length(item), 1)
   expect_equal(item[[1]]$user$id, "yutannihilation")
@@ -16,6 +20,8 @@ test_that("qiita_get_items by item ID", {
 
 test_that("qiita_get_items by item ID", {
   skip_on_cran()
+  skip_on_ci()
+
   item <- qiita_get_items(user_id = "yutannihilation")
   expect_true("7a78d897810446dd6a3b" %in% purrr::map_chr(item, "id"))
   expect_true(all(purrr::map_chr(item, c("user", "id")) == "yutannihilation"))
--- tests/testthat/test-qiita_get_users.R
+++ tests/testthat/test-qiita_get_users.R
@@ -2,6 +2,8 @@ context("qiita_get_users")
 
 test_that("qiita_get_users works", {
   skip_on_cran()
+  skip_on_ci()
+
   users <- qiita_get_users("yutannihilation")
   expect_equal(length(users), 1)
   expect_equal(users[[1]]$id, "yutannihilation")
@@ -9,6 +11,8 @@ test_that("qiita_get_users works", {
 
 test_that("qiita_is_following_user works", {
   skip_on_cran()
+  skip_on_ci()
+
   expect_true(qiita_is_following_user("uri"))
   # I don't follow myself :)
   expect_false(qiita_is_following_user("yutannihilation"))
@@ -16,5 +20,7 @@ test_that("qiita_is_following_user works", {
 
 test_that("qiita_get_authenticated_user works", {
   skip_on_cran()
+  skip_on_ci()
+
   expect_equal(qiita_get_authenticated_user()$id, "yutannihilation")
 })
